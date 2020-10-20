e09456ea4123c3a49196a395cc74b0b3d001f2e4
Tung-Neo <62163195+Tung-Neo@users.noreply.github.com>
2020-10-20 10:45:32 +0700

    lap07

--- U44/HamburgerAdaptiveLayout/Model/NewsItem.cs
+++ U44/HamburgerAdaptiveLayout/Model/NewsItem.cs
@@ -13,7 +13,7 @@ namespace HamburgerAdaptiveLayout.Model
         public string Category { get; set; }
         public string Headline { get; set; }
         public string Subhead { get; set; }
-        public string DataLine { get; set; }
+        public string DateLine { get; set; }
         public string Image { get; set; }
     }
 
@@ -23,7 +23,7 @@ namespace HamburgerAdaptiveLayout.Model
             string category,
             ObservableCollection<NewsItem> newsItems)
         {
-            var allItems = GetNewsItems();
+            var allItems = getNewsItems();
 
             var filteredNewsItems = allItems
                 .Where(p => p.Category == category)
@@ -33,17 +33,23 @@ namespace HamburgerAdaptiveLayout.Model
 
             filteredNewsItems.ForEach(p => newsItems.Add(p));
         }
-        private static List<NewsItem> GetNewsItems()
+        private static List<NewsItem> getNewsItems()
         {
             var items = new List<NewsItem>();
 
-            items.Add(new NewsItem() { Id = 1, Category = "Financial", Headline = "Abc", Subhead = "Xyz", DataLine = "123", Image = "Assets/neo-cartoon.jpg" });
-            items.Add(new NewsItem() { Id = 2, Category = "Financial", Headline = "Abc", Subhead = "Xyz", DataLine = "123", Image = "Assets/neo-cartoon.jpg" });
-            items.Add(new NewsItem() { Id = 3, Category = "Financial", Headline = "Abc", Subhead = "Xyz", DataLine = "123", Image = "Assets/neo-cartoon.jpg" });
+            items.Add(new NewsItem() { Id = 1, Category = "Financial", Headline = "Abc", Subhead = "Xyz", DateLine = "123", Image = "Assets/neo-2019.jpg" });
+            items.Add(new NewsItem() { Id = 2, Category = "Financial", Headline = "Abc", Subhead = "Xyz", DateLine = "123", Image = "Assets/neo-2019.jpg" });
+            items.Add(new NewsItem() { Id = 3, Category = "Financial", Headline = "Abc", Subhead = "Xyz", DateLine = "123", Image = "Assets/neo-2019.jpg" });
+            items.Add(new NewsItem() { Id = 4, Category = "Financial", Headline = "Abc", Subhead = "Xyz", DateLine = "123", Image = "Assets/neo-2019.jpg" });
+            items.Add(new NewsItem() { Id = 5, Category = "Financial", Headline = "Abc", Subhead = "Xyz", DateLine = "123", Image = "Assets/neo-2019.jpg" });
+            items.Add(new NewsItem() { Id = 6, Category = "Financial", Headline = "Abc", Subhead = "Xyz", DateLine = "123", Image = "Assets/neo-2019.jpg" });
 
-            items.Add(new NewsItem() { Id = 4, Category = "Food", Headline = "Abc", Subhead = "Xyz", DataLine = "123", Image = "Assets/neo-2018.jpg" });
-            items.Add(new NewsItem() { Id = 5, Category = "Food", Headline = "Abc", Subhead = "Xyz", DataLine = "123", Image = "Assets/neo-2018.jpg" });
-            items.Add(new NewsItem() { Id = 6, Category = "Food", Headline = "Abc", Subhead = "Xyz", DataLine = "123", Image = "Assets/neo-2018.jpg" });
+            items.Add(new NewsItem() { Id = 7, Category = "Food", Headline = "Abc", Subhead = "Xyz", DateLine = "123", Image = "Assets/neo-2018.jpg" });
+            items.Add(new NewsItem() { Id = 8, Category = "Food", Headline = "Abc", Subhead = "Xyz", DateLine = "123", Image = "Assets/neo-2018.jpg" });
+            items.Add(new NewsItem() { Id = 9, Category = "Food", Headline = "Abc", Subhead = "Xyz", DateLine = "123", Image = "Assets/neo-2018.jpg" });
+            items.Add(new NewsItem() { Id = 10, Category = "Food", Headline = "Abc", Subhead = "Xyz", DateLine = "123", Image = "Assets/neo-2018.jpg" });
+            items.Add(new NewsItem() { Id = 11, Category = "Food", Headline = "Abc", Subhead = "Xyz", DateLine = "123", Image = "Assets/neo-2018.jpg" });
+            items.Add(new NewsItem() { Id = 12, Category = "Food", Headline = "Abc", Subhead = "Xyz", DateLine = "123", Image = "Assets/neo-2018.jpg" });
 
             return items;
         }
